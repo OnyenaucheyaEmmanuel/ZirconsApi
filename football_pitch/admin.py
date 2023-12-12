5d20bcf2e86d2b2910396403259b7d82c7435671
Onyenaucheya Emmanuel <94042276+OnyenaucheyaEmmanuel@users.noreply.github.com>
2023-12-12 09:06:24 +0100

    another commit

--- football_pitch/admin.py
+++ football_pitch/admin.py
@@ -5,7 +5,7 @@ from .models import Payment
 
 
 class PaymentAdmin(admin.ModelAdmin):
-	list_display = ["id", 'first_name','last_name','email', 'players','team_name','phone','start_time','date', 'amount', "verified", "ref","date_created",]
-	# search_fields = ['first_name','last_name','email' 'team_name','phone',]
+    list_display = ["id", 'first_name', 'last_name', 'email', 'players', 'team_name', 'phone', 'start_time', 'date', 'amount', "verified", "ref", "date_created",]
+    search_fields = ['first_name', 'last_name', 'email', 'team_name', 'phone',]
 
 admin.site.register(Payment, PaymentAdmin)
