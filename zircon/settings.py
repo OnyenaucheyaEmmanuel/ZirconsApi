fdd20b635280e1e2233f0fe8c4480e7575da6509
Onyenaucheya Emmanuel <94042276+OnyenaucheyaEmmanuel@users.noreply.github.com>
2023-12-14 09:12:18 +0100

    another commit

--- zircon/settings.py
+++ zircon/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = 'static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATICFILES_DIRS = 'static/'
-STATIC_ROOT = "static/"
+STATIC_ROOT = BASE_DIR/'assets'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
