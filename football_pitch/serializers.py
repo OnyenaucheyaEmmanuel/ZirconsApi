d032ffe60ee5c575a2fa7204e3d2f30e80e22700
Onyenaucheya Emmanuel <94042276+OnyenaucheyaEmmanuel@users.noreply.github.com>
2023-12-20 19:53:58 +0100

    Update serializers.py

--- football_pitch/serializers.py
+++ football_pitch/serializers.py
@@ -1,8 +1,11 @@
-# booking/serializers.py
+# football_pitch/serializers.py
 from rest_framework import serializers
-from .models import Payment
+
+from football_pitch.models import Payment
+
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
+        ref_name = 'football_pitch_payment'
